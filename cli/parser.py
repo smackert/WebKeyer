8dfd2a325e4a7bf8b76087ea8cd1325ee5ad0dcd
Sean Mackert <smackert@users.noreply.github.com>
2023-04-02 20:00:00 +0400

    Change default behavior of verify_ssl
    Change verify_ssl to default=None to prevent overwrites of downstream
    defaults in web.get_webpage()

--- cli/parser.py
+++ cli/parser.py
@@ -6,7 +6,7 @@ def get_parser():
     parser.add_argument('-i', '--input-file', dest='input_file', help='The file which will be encrypted/decrypted')
     parser.add_argument('-u', '--url', dest='url', help='The URL which will be used to generate the key')
     parser.add_argument('-ih', '--include-headers', dest='include_headers', action='store_true', help='Include response headers in keyfile generation')
-    parser.add_argument('-k', '--insecure', dest='verify_ssl', action='store_false', help='Ignore SSL checks')
+    parser.add_argument('-k', '--insecure', dest='verify_ssl', action='store_false', default=None, help='Disable SSL checks')
     parser.add_argument('-o', '--output', dest='output_path', help='Output file path')
     parser.add_argument("-v", "--verbose", dest='verbose', action='store_true', help="increase output verbosity")
     return parser
