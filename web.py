f5cbe3bb7a663b24dd26b91cc5fc354bd32b987f
Sean Mackert <smackert@users.noreply.github.com>
2023-04-02 18:09:30 +0400

    Handle optional arguments with defaults.

--- web.py
+++ web.py
@@ -1,7 +1,9 @@
 import requests
 import logging
 
-def get_webpage(url, verify_ssl=True, include_headers=False):
+def get_webpage(url, **request_options):
+    verify_ssl = request_options.get('verify_ssl', True)
+    include_headers = request_options.get('include_headers', False)
     r = requests.get(url, verify=verify_ssl)
     if not include_headers:
         return r.text.encode('utf-8')
