d756ab59efa48e47228ce24a703cfa93940b9892
Sean Mackert <smackert@users.noreply.github.com>
2023-04-02 18:54:42 +0400

    Added input_file flag

--- cli/parser.py
+++ cli/parser.py
@@ -3,6 +3,7 @@ import logging
 
 def get_parser():
     parser = argparse.ArgumentParser(description='Encrypt Files with Web-Based Keyfiles')
+    parser.add_argument('-i', '--input-file', dest='input_file', help='The file which will be encrypted/decrypted')
     parser.add_argument('-u', '--url', dest='url', help='The URL which will be used to generate the key')
     parser.add_argument('-ih', '--include-headers', dest='include_headers', help='Include response headers in keyfile generation')
     parser.add_argument('-k', '--insecure', dest='verify_ssl', help='Ignore SSL checks')
