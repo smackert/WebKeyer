6710603ed85ec94c91d807ad85ca78ae6200d23b
Sean Mackert <smackert@users.noreply.github.com>
2023-04-06 15:23:04 +0400

    refactor: move input-file err handling to argparse
    - Removed error handling code for input file validation in `main` and
      added the `required` argument to the `input_file` option in `argparse`

--- cli/parser.py
+++ cli/parser.py
@@ -6,7 +6,7 @@ def get_parser():
     mode_group = parser.add_mutually_exclusive_group(required=True)
     mode_group.add_argument('-e', '--encrypt', dest='encrypt_mode', action='store_true', help='Encrypt the input file')
     mode_group.add_argument('-d', '--decrypt', dest='decrypt_mode', action='store_true', help='Decrypt the input file')
-    parser.add_argument('-i', '--input-file', dest='input_file', help='The file which will be encrypted/decrypted')
+    parser.add_argument('-i', '--input-file', dest='input_file', type=argparse.FileType('r'), help='The file which will be encrypted/decrypted')
     parser.add_argument('-u', '--url', dest='url', help='The URL which will be used to generate the key')
     parser.add_argument('-ih', '--include-headers', dest='include_headers', action='store_true', help='Include response headers in keyfile generation')
     parser.add_argument('-k', '--insecure', dest='verify_ssl', action='store_false', default=None, help='Disable SSL checks')
