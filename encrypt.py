a0eeff11da7008c590706d2e3aaaf20654c066fe
Sean Mackert <smackert@users.noreply.github.com>
2023-05-04 11:42:39 +0400

    fix: error message on reading input-file

--- encrypt.py
+++ encrypt.py
@@ -6,13 +6,13 @@ import base64
 import logging
 import sys
 
+
 def encrypt(password, input_file, output_path):
-    
     # Generate Salt
     salt = os.urandom(16)
 
     # Generate Key
-    key = hashlib.pbkdf2_hmac('sha256', password.encode(), salt, 100000)
+    key = hashlib.pbkdf2_hmac("sha256", password.encode(), salt, 100000)
 
     # Create Fernet object
     # Fernet key must be 32 url-safe base64-encoded bytes.
@@ -21,17 +21,17 @@ def encrypt(password, input_file, output_path):
 
     # Encrypt file
     try:
-        with input_file.open('rb') as f_in:
+        with input_file.open("rb") as f_in:
             try:
                 enc_d = f.encrypt(f_in.read())
             except Exception as e:
-                sys.exit(f'Failed to encrypt data.\n{e}')
+                sys.exit(f"Failed to encrypt data.\n{e}")
     except:
-        sys.exit("Failed to open input file.\n{e}")
+        sys.exit(f"Failed to open input file: {input_file}\n")
 
     # Write file
     try:
-        with output_path.open('wb') as f_out:
+        with output_path.open("wb") as f_out:
             f_out.write(salt)
             f_out.write(enc_d)
     except Exception as e:
